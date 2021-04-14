c4f6333c8e0a63a9c5fdd1e2f987a68793e7cbf7
tleung22 <trevor22@uw.edu>
2021-04-13 18:30:11 -0700

    added style and some content to home page

--- my-app/src/components/GetStarted.js
+++ my-app/src/components/GetStarted.js
@@ -13,11 +13,32 @@ export default class GetStarted extends React.Component {
     render() {
         return (
             <div>
-                <button>
-                    <Link to="/tutorial">Get Started</Link>
-                </button>
-                <p> swag </p>
-
+                <div id="general-section">
+                    <p>Being aware about your online behavior <strong>starts here.</strong></p>
+                    <button>
+                        <Link to="/Tutorial">Get Started</Link>
+                    </button>
+                </div>
+                <div id="sections">
+                    <div id="upload" class="section">
+                        <h3>Upload</h3>
+                        <button class="section-buttons">
+                            <Link to="/upload">Upload</Link>
+                        </button>
+                    </div>
+                    <div id="understand" class="section">
+                        <h3>Understand</h3>
+                        <button class="section-buttons">
+                            <Link to="/">Understand</Link>
+                        </button>
+                    </div>
+                    <div id="evaluate" class="section">
+                        <h3>Evaluate</h3>
+                        <button class="section-buttons">
+                            <Link to="/">Evaluate</Link>
+                        </button>
+                    </div>
+                </div>
             </div>
         )
     }
